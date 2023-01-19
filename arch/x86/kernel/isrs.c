648cf9b4273fcd055000a0f398cb4ed00333733d
Aziiz44 <3z.zhtan44@gmail.com>
2023-01-19 23:19:24 +0000

    kernel changed by Abdulaziz

--- arch/x86/kernel/isrs.c
+++ arch/x86/kernel/isrs.c
@@ -914,6 +914,20 @@ void syscall_handler(struct state *s)
 			break;
 #endif
 
+#ifndef DISABLE_SYS_TRUNCATE
+        case 320:
+            s->rax = sys_truncate((const char*)s->rdi, s->rsi);
+            break;
+#endif
+
+ 
+
+#ifndef DISABLE_SYS_FTRUNCATE
+        case 330:
+            s->rax = sys_truncate(s->rdi, s->rsi);
+            break;
+#endif
+
 #ifndef DISABLE_SYS_RSEQ
 		case 334:
 			/* rseq */
